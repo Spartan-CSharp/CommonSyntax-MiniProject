8751ec9ce4ec1ac51adeefdb10e8c93c826f86ab
Pierre Plourde <pierre@spartancsharp.net>
2024-02-07 12:29:15 -0500

    Create Code
    Planned and built a Console Application that asks a user for their name and their age. If their name is Bob or Sue, address them as professor. If the person is under 21, recommend they wait X years to start this class.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,6 +10,37 @@ namespace ConsoleUI
 	{
 		static void Main(string[] args)
 		{
+			Console.Write("What is your first name: ");
+			string firstname = Console.ReadLine();
+
+			Console.Write("How old are you: ");
+			string agetext = Console.ReadLine();
+
+			bool isagevalid = int.TryParse(agetext, out int age);
+
+			if ( firstname.ToLower() == "bob" || firstname.ToLower() == "sue" )
+			{
+				Console.WriteLine($"Hello Professor {firstname}");
+			}
+			else
+			{
+				Console.WriteLine($"Hello {firstname}");
+			}
+
+			if ( isagevalid )
+			{
+				if ( age < 21 )
+				{
+					int yearstowait = 21 - age;
+					Console.WriteLine($"I recommend you wait {yearstowait} years before starting this class");
+				}
+			}
+			else
+			{
+				Console.WriteLine("You must enter your age in digits");
+			}
+
+			_ = Console.ReadLine();
 		}
 	}
 }
